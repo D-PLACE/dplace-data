ffd341ec98de17cdb030fd24eea05b78e810fc2a
xrotwang <xrotwang@googlemail.com>
2016-02-03 11:23:04 +0100

    cleanup

--- scripts/glottolog.py
+++ scripts/glottolog.py
@@ -36,48 +36,6 @@ def clean_tree(tree):  # pragma: no cover
     return True
 
 
-SQL = """\
-select distinct
-  l.id,
-  l.name,
-  l.level,
-  l.status,
-  l.fid,
-  l.latitude,
-  l.longitude,
-  iso.name
-from
-  (
-    select
-      l.pk,
-      l.id,
-      l.name,
-      ll.level,
-      ll.status,
-      f.id as fid,
-      f.name as fname,
-      l.latitude,
-      l.longitude
-    from
-      language as l,
-      languoid as ll,
-      language as f
-    where
-      l.pk = ll.pk and ll.family_pk = f.pk and l.active = TRUE
-  ) as l
-left outer join
-  (
-    select
-      i.name, li.language_pk
-    from
-      languageidentifier as li, identifier as i
-    where
-      li.identifier_pk = i.pk and i.type = 'iso639-3'
-  ) as iso on (iso.language_pk = l.pk)
-order by l.id;
-"""
-
-
 GLOTTOLOG_FAMILIES = "http://glottolog.org/glottolog/language.atom?type=families&sSearch_1=Top-level+unit"
 SUFFIX = '.glotto.trees'
 
