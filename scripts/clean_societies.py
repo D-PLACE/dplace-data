f5f2deb94f46f31919d68671d9a71e8e81f25750
xrotwang <xrotwang@googlemail.com>
2017-03-09 22:40:07 +0100

    updated glottolog languoid data to what will become glottolog 3.0

--- scripts/clean_societies.py
+++ scripts/clean_societies.py
@@ -9,47 +9,48 @@ import re
 from clldutils.path import Path
 from clldutils.dsv import reader, UnicodeWriter
 from clldutils.text import split_text
+from clldutils.misc import slug
 from pyglottolog.api import Glottolog
 
 
+biome_map = {
+    "Mediterranean Forests, Woodlands & Scrub":
+        "Mediterranean forests, woodlands, and scrub or sclerophyll forests",
+    "Temperate Conifer Forests":
+        "Temperate coniferous forests",
+    "Mangroves": "Mangrove",
+    "Missing data": "NA",
+}
+
+
 def main(data_dir):
-    equiv_pattern = re.compile('(?P<dsid>[^_]+)_society_equivalent')
-    locations = {
-        r['soc_id']: r for r in reader(data_dir.joinpath('csv', 'LatLong_data.csv'), dicts=True)}
-    xd2gc = {r['xd_id']: r['DialectLanguageGlottocode']
-             for r in reader(data_dir.joinpath('csv', 'xd_id_to_language.csv'), dicts=True)}
-    for d in data_dir.joinpath('datasets').iterdir():
-        if d.name in ['EA', 'Binford']:
-            rows = list(reader(d.joinpath('societies.csv'), dicts=True))
-            for row in rows:
-                if row['xd_id'] in xd2gc:
-                    del xd2gc[row['xd_id']]
+    codes = {}
+    for var in ['Biome', 'EcoRegion']:
+        codes[var] = {
+            slug(biome_map.get(r['description'], r['description']).replace('&', 'and')): r
+            for r in reader(data_dir.joinpath('datasets', 'TEOW', 'codes.csv'), dicts=True)
+            if r['var_id'] == var}
 
-    for k, v in xd2gc.items():
-        print(k, v)
-    return
-
-    if 1:
-        if d.is_dir() and d.joinpath('societies.csv').exists():
-            societies = list(reader(d.joinpath('societies.csv'), dicts=True))
-            cols = list(societies[0].keys())
-            equivalents = {i: equiv_pattern.match(n).group('dsid')
-                           for i, n in enumerate(cols) if equiv_pattern.match(n)}
-            header = [c for i, c in enumerate(cols) if i not in equivalents]
-            loc_cols = 'origLat,origLong,Lat,Long,Comment'.split(',')
-            header.extend(loc_cols)
-            with UnicodeWriter(d.joinpath('societies.csv')) as writer, \
-                    UnicodeWriter(d.joinpath('societies_mapping.csv')) as rwriter:
-                writer.writerow(header[1:])
-                rwriter.writerow(['id', 'related'])
-                for soc in societies:
-                    loc = locations.get(soc['soc_id'], {})
-                    equivs = ['{0}:{1}'.format(ds, soc.values()[i])
-                              for i, ds in equivalents.items() if soc.values()[i]]
-                    rwriter.writerow([soc['soc_id'], ';'.join(equivs)])
-                    row = [v for i, v in enumerate(soc.values()) if i not in equivalents][1:]
-                    row.extend([loc.get(c) for c in loc_cols])
-                    writer.writerow(row)
+    with UnicodeWriter(data_dir.joinpath('datasets', 'TEOW', 'data.csv')) as writer:
+        writer.writerow('dataset, soc_id, sub_case, year, var_id, code, comment, references, source_coded_data, admin_comment'.split(', '))
+        for r in reader(data_dir.joinpath('csv', 'environmental_data.csv'), dicts=True):
+            if r['VarID'] in ['OlsonBiome', 'OlsonEcoRegion']:
+                code = slug(r['Code'].replace('\xa0', ' ').strip())
+                if code not in codes[r['VarID'].replace('Olson', '')]:
+                    print(r)
+                else:
+                    writer.writerow([
+                        r['Dataset'] if r['Dataset'] != 'Jorgensen' else 'WNAI',
+                        r['soc_id'],
+                        '',
+                        '1988' if r['VarID'] == 'OlsonBiome' else '1981',
+                        r['VarID'].replace('Olson', ''),
+                        codes[r['VarID'].replace('Olson', '')][code]['code'],
+                        r['Comment'],
+                        '',
+                        '',
+                        ''
+                    ])
 
 
 if __name__ == "__main__":
