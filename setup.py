c607844284772e4d5275a326ba80b673b29e5797
xrotwang <xrotwang@googlemail.com>
2017-04-20 12:49:30 +0200

    don't include all data in the python package - since this results in a >40 MB package

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='pydplace',
-    version='0.2',
+    version='0.3',
     license='CC-BY-4.0',
     description='programmatic access to D-PLACE/dplace-data',
     long_description='',
